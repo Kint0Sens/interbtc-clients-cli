3346baee67e6a920253ca3c3c5b2c6bea81de349
claudiociardell <claudio.ciardelli@gmail.com>
2022-05-20 08:05:58 +0000

    wip

--- premium_bot/pay_bot/src/main.rs
+++ premium_bot/pay_bot/src/main.rs
@@ -58,18 +58,42 @@ struct Cli {
 
 #[derive(Parser, Clone)]
 pub struct ToolConfig {
-    /// Vault to issue from - account
+    /// Vault to be used for issue
+    /// If left empty, a random eligible vault will be selected
     #[clap(long)]
-    vault_account_id: AccountId,
+    force_vault_account_id: AccountId,
 
-    /// Vault to issue to - collateral
-    #[clap(long, default_value = "KSM")] 
-    vault_collateral_id: String,
+    /// Max Amount to issue, in satoshis, 
+    /// must be greater than Bridge Fee + BTC Network Fee + BTC Dust Limit 
+    #[clap(long, validator = amount_gt_minimal, default_value = "999999999999999999999")]
+    max_issue_amount: u128,
 
-    /// Vault to issue to
-    #[clap(long, default_value = "KBTC")] 
-    vault_wrapped_id: String,
-}
+    /// Min Amount to issue, in satoshis, 
+    /// must be greater than Bridge Fee + BTC Network Fee + BTC Dust Limit 
+    #[clap(long, validator = amount_gt_minimal, default_value = "2000")]
+    max_issue_amount: u128,
+
+    /// Minimum btc wallet amount in sat, 
+    /// bot will not trigger issue when balance is below this amount
+    #[clap(long, default_value = "2000")]
+    min_btc_balance: u128,
+
+    /// Sleep time before checking for available vault again
+    #[clap(long, default_value = "15")]
+    sleeptime_no_issuable_vault: u64,
+
+    /// Sleep time before checking for available BTC again
+    #[clap(long, default_value = "15")]
+    sleeptime_not_enough_btc: u64,
+
+    /// Collateral
+     #[clap(long, default_value = "KSM")]  // Make network dependent default
+     chain_collateral_id: String,
+ 
+     /// Wrapped
+     #[clap(long, default_value = "KBTC")] // Make network dependent default
+     chain_wrapped_id: String,
+ }
 
 #[tokio::main]
 #[allow(unreachable_code)]
@@ -77,17 +101,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli: Cli = Cli::parse();
     env_logger::init_from_env(init_logger(cli.verbose));
 
-    if cli.testmode > 0 {
-        tracing::info!("Running in test mode, not signing transactions");
-    }
+    let config = cli.config;
 
-     //Main loop
+    //Main loop
     // Check available btc balance
     // Identify Vault with issuable capacity
     // Request Issue
     // Pay Issue
     // Report balance
     // repeat
+
+    let amount = config.amount;
+    // let btc_address : BtcAddress = BtcAddress::decode_str(&config.btc_address).unwrap();
+    let collateral_id  = parse_collateral_currency(&config.vault_collateral_id).unwrap();
+    let wrapped_id  = parse_wrapped_currency(&config.vault_wrapped_id).unwrap();
+    let force_vault = if config.vault_account_id.is_empty() == true {
+        true 
+    } else {
+        false
+    };
+    if force_vault == true { 
+        let vault_id = VaultId::new(config.vault_account_id, collateral_id, wrapped_id);
+    };
 
     // User keys
     let (key_pair, _) = cli.account_info.get_key_pair()?;
@@ -102,8 +137,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let parachain = parachain_config.try_connect(signer.clone(), shutdown_tx.clone()).await?;
     tracing::info!("TEXT_CONNECTED");
 
+
     tracing::info!("Signer:           {}",signer_account_id.to_ss58check());
-   
+
+    tracing::info!("Selected vault:            {}",vault_id.account_id.to_ss58check());
+    tracing::info!("Issue amount:     {} {} Sat",config.amount, config.chain_wrapped_id);
+    tracing::info!("Issuable amount:  {} {} Sat",issuable_amount, config.chain_wrapped_id);
+
+  
     // Setup wallet
     let external_descriptor = "wpkh(tprv8ZgxMBicQKsPctgasNzABhRCAfReohQPdu235WxXhu7yuh3by91GhqZgRGN6GEdARTEWJ2iURcjtbAub8ifnzbym5vGs4V54DwK8VL9b9oZ/84'/0'/0'/0/*)";
     let internal_descriptor = "wpkh(tprv8ZgxMBicQKsPctgasNzABhRCAfReohQPdu235WxXhu7yuh3by91GhqZgRGN6GEdARTEWJ2iURcjtbAub8ifnzbym5vGs4V54DwK8VL9b9oZ/84'/0'/0'/1/*)";
@@ -118,82 +159,98 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let address = wallet.get_address(AddressIndex::New)?;
     // tracing::info!("Generated Address: {}", address);
 
-    tracing::info!("Synching local BTC wallet");
-    wallet.sync(noop_progress(), None)?;
-    let balance = wallet.get_balance()?;
-    tracing::info!("BTC Wallet balance in SAT: {}", balance);
-    let mut paid_issues = Vec::new();
-    let mut found_one: bool;
+    // Main loop
     loop {
-        found_one = false;
-        let issue_requests = parachain.get_all_active_issues().await?;
-        tracing::debug!("Found {} issues on target vault", issue_requests.len());
-        for (issue_id, request) in issue_requests.into_iter() {
-            if request.requester == signer_account_id {
-                found_one = true;   
-                tracing::info!("found issue id:{} for specified signer",issue_id);
-                if paid_issues.contains(&issue_id) {
-                    tracing::info!("issue id:{} - already paid, skipping",issue_id);
-                } else {
-                    tracing::info!("request_status:{:?}",request.status);
-                    tracing::info!("request_btc:{:?}",request.btc_address);
-                    // tracing::info!("request_btc_public_key:{:?}",request.btc_public_key);
-                    tracing::info!("request_requester:{}",request.requester);
-                    let amount : u64 = request.amount as u64;  // no checks, I do not have that many BTC
-                    let fee : u64 = request.fee as u64;  // no checks, as above
-                    tracing::info!("request_amount:{}, request_fee:{}",amount,fee);
-                    let issue_request = parachain.get_issue_request(issue_id).await?;
-                    let issue_request_btc_address_str = issue_request.btc_address.encode_str(BITCOIN_NETWORK).unwrap();
-                    let issue_request_btc_address = Address::from_str(&issue_request_btc_address_str)?; 
-                    tracing::info!("btc_address:{:?}",issue_request_btc_address_str);
-        
-                    tracing::info!("Synching wallet");
-                    wallet.sync(noop_progress(), None)?;
-                    let balance = wallet.get_balance()?;
-                    tracing::info!("Wallet balance in SAT: {}", balance);
+        // Check available btc balance
+        tracing::info!("Synching wallet");
+        wallet.sync(noop_progress(), None)?;
+        let balance = wallet.get_balance()?;
+        tracing::info!("Wallet balance in SAT: {}", balance);
+        if balance < min_btc_balance {
+            // Not enough BTC. Sleep and retry later.
+            tracing::warn!("Not enough BTC balance.");
+            tracing::info!("Waiting {} seconds before checking again", config.sleeptime_not_enough_btc);
+            thread::sleep(Duration::from_secs(config.sleeptime_not_enough_btc));
+            continue;        
+        };
 
-                    let tx_amount = amount + fee;
-
-                    if balance < tx_amount {
-                        tracing::info!("Balance too low. Cancelling payment");
-                    } else {
-
-                        let mut tx_builder = wallet.build_tx();
-                        tx_builder
-                            .add_recipient(issue_request_btc_address.script_pubkey(), tx_amount)
-                            .enable_rbf();
-                        let (mut psbt, tx_details) = tx_builder.finish()?;
-                        tracing::info!("Transaction details: {:#?}", tx_details);
-                        // Do not sign in test mode
-                        if cli.testmode > 0 {
-                            tracing::info!("Test mode. Not signing transaction");
-                        } else {
-                            tracing::info!("Signing transaction");
-                            let finalized = wallet.sign(&mut psbt, SignOptions::default())?;
-                            assert!(finalized, "Tx has not been finalized");
-                            // tracing::info!("Transaction Signed: {}", finalized);
-                            let raw_transaction = psbt.extract_tx();
-                            let txid = wallet.broadcast(&raw_transaction)?;
-                            tracing::info!(
-                                "Transaction sent! TXID: {txid}.\nExplorer URL: https://blockstream.info/testnet/tx/{txid}",
-                                txid = txid
-                            );
-                        }
-                        paid_issues.push(issue_id);
+        // Identify Vault with issuable capacity (or use forced vault)
+        let vault_id;
+        if force_vault == true {
+            vault_id = force_vault_id;
+        } else {
+            // Get eligible vaults
+            // Select one with issueable amount > max_issue_amount is found
+            // else take the one with greatest issuable amt
+            let vaults : Vec<_> = parachain.get_all_vaults().await?;
+            let current_height = parachain.get_current_active_block_number().await?;
+            let mut max_issuable_amt = 0; 
+            let mut max_issuable_vault : VaultId;
+            for vault in vaults.iter() {
+                match vault.status {
+                    VaultStatus::Active(active) => {
+                        if active == false { // Vault set to inactive, does not accept issue requests
+                            continue; 
+                        };
+                        let loop_issuable_amount: u128 =  parachain.get_issuable_tokens_from_vault(vault.clone()).await?;
+                        if max_issuable_amt <= loop_issuable_amt.amount {
+                            max_issuable_amt = loop_issuable_amt.amount;
+                            max_issuable_vault = vault.clone();
+                        }; 
+                                    
                     }
-                }
-            } else {
-                tracing::debug!("issue id:{} - other signer: {}",issue_id,request.requester);
-
+                    _ => {},
             }
+
+            if max_issuable_amt > config.min_issue_amount {
+                vault_id = max_issuable_vault;
+                tracing::info!("Selected vault {} with issuable amount of {} {}",
+                                vault_id.id.account_id.pretty_print(),
+                                max_issuable_amt,
+                                wrapped_id );
+            } else {
+                // No vault found to execute issue. Sleep and retry later
+                tracing::warn!("No vault available with issuable amount above minimum issue amount");
+                tracing::info!("Waiting {} seconds before checking again", config.sleeptime_no_issuable_vault);
+                thread::sleep(Duration::from_secs(config.sleeptime_no_issuable_vault));
+                continue;
+            };
+
         }
-        if found_one == false {
-            tracing::info!("...Listenting to vault...");
-        }
-    };
+    
+        // Emit Issue Request
+        let issue_amount = if max_issue_amount > max_issuable_amt { max_issuable_amt } else { max_issue_amount };
+        let issue = parachain.request_issue(issue_amount, &vault_id).await?;
+        tracing::info!("Issue request accepted");
+        tracing::info!("BTC address:      {:?}",issue.vault_address);
+        tracing::info!("                  {:?}",issue.vault_address.encode_str(BITCOIN_NETWORK));
+        
+        tracing::info!("Issue amount:     {} {} Sat",issue.amount, config.vault_wrapped_id);
+        tracing::info!("Issue fee:        {} {} Sat",issue.fee, config.vault_wrapped_id);
+
+        // Send BTC transaction
+        let tx_amount = issue.amount + issue.fee;
+        let mut tx_builder = wallet.build_tx();
+        tx_builder
+            .add_recipient(issue_request_btc_address.script_pubkey(), tx_amount)
+            .enable_rbf();
+        let (mut psbt, tx_details) = tx_builder.finish()?;
+        tracing::info!("Transaction details: {:#?}", tx_details);
+        tracing::info!("Signing transaction");
+        let finalized = wallet.sign(&mut psbt, SignOptions::default())?;
+        assert!(finalized, "Tx has not been finalized");
+        // tracing::info!("Transaction Signed: {}", finalized);
+        let raw_transaction = psbt.extract_tx();
+        let txid = wallet.broadcast(&raw_transaction)?;
+        tracing::info!(
+            "Transaction sent! TXID: {txid}.\nExplorer URL: https://blockstream.info/testnet/tx/{txid}",
+            txid = txid
+        );
+
+    }
 
     Ok(())
      
-    }
+}
 
   
